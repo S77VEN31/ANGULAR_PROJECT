7666f38670d367e19724045e1813418f47641eb9
S77VEN31 <stivensg313131@outlook.com>
2023-06-20 04:29:52 -0600

    add: ingredients component

--- src/app/styleguide/components/pages/ingredients/ingredients.component.ts
+++ src/app/styleguide/components/pages/ingredients/ingredients.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
+import { Ingredient } from './ingredient';
 @Component({
   selector: 'app-ingredients',
   templateUrl: './ingredients.component.html',
   styleUrls: ['./ingredients.component.css']
 })
 export class IngredientsComponent {
-
+  data = {
+    SHOPPING_LIST: 'Shopping List',
+  };
+  SHOPPING_LIST: string = this.data.SHOPPING_LIST;
+  ingredients:  Ingredient[] = Ingredient.ingredientFactory();
 }
